655352e584d8f420bc4ac862fd849d36e8d95076
Z-thrones <zaviyar.ktk@gmail.com>
2026-03-02 15:48:11 +0500

    Add files via upload

---
+++ A7.py
@@ -0,0 +1,10 @@
+print("There are 5 trees in jacks backyard. jack measures each one of the and writes them down on a list. here are his numbers, 98,94,41,96 and 11. We are going to find out the average of his measurements")
+t1 = 98
+t2 = 94
+t3 = 41
+t4 = 96
+t5 = 11
+sum = t1+t2+t3+t4+t5
+average = sum / 5
+print("The average of all five trees is" , average)
+print("The sum of all five trees is" , sum)
